907994ba1250aec495375db46a31626319d8ff30
OteroSantiago <91058589+OteroSantiago@users.noreply.github.com>
2021-10-12 21:27:18 +0200

    Initial commit

---
+++ src/com/ceep/Funcionalidad.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ceep;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Funcionalidad {
+    public static void imprimir(String s){
+        System.out.println("s = "+s);
+    }
+}
---
+++ src/paquetes/Paquetes.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquetes;
+import com.ceep.*;
+import static com.ceep.Funcionalidad.imprimir;//para la linea 20
+/**
+ *
+ * @author Santiago
+ */
+public class Paquetes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Funcionalidad.imprimir("Saludos");
+        imprimir("adios");
+    }
+    
+}
